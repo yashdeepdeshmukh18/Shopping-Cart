d1800f88fe52bbc6d0f982da499d4b114294bf02
yashdeepdeshmukh18 <ybdeshmukh123@gmail.com>
2026-05-16 15:34:46 +0530

    removed unused varaibles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { FaShoppingCart } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
-import Cart from "../pages/Cart";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
